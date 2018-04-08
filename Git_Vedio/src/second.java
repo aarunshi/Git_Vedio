02a9fc9278afeee32d080706f0b2e380b51c5493
kumud malhotra <kumudmalhotra@gmail.com>
2018-04-08 20:22:26 +0530

    branch 1 commit hi

--- Git_Vedio/src/second.java
+++ Git_Vedio/src/second.java
@@ -2,7 +2,7 @@
 public class second {
 	public static void main(String args[])
 	{
-		System.out.println("hi");
+		System.out.println("hi new");
 		System.out.println("in branch1 with conflict");
 	}
 
