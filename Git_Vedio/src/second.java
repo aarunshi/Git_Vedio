04033c49d5f57b32f7968a57289309ad9a2632b2
kumud malhotra <kumudmalhotra@gmail.com>
2018-04-08 22:23:49 +0530

    aa

--- Git_Vedio/src/second.java
+++ Git_Vedio/src/second.java
@@ -2,14 +2,9 @@
 public class second {
 	public static void main(String args[])
 	{
-
 		System.out.println("hi new");
 		System.out.println("in branch1 with conflict");
-<<<<<<< HEAD
-
-=======
 		System.out.println("third");
->>>>>>> refs/heads/Branch1
 	}
 
 
