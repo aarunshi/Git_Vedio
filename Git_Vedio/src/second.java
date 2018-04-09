1b1b91bae498949eafa527741517b8081f9231e0
aarunshi <38101661+aarunshi@users.noreply.github.com>
2018-04-09 22:27:50 +0530

    Update second.java

--- Git_Vedio/src/second.java
+++ Git_Vedio/src/second.java
@@ -2,10 +2,8 @@
 public class second {
 	public secondmethod()
 	{
-		System.out.println("hi new");
-		System.out.println("in branch1 with conflict");
-		System.out.println("in branch1 with conflict");
-		System.out.println("third33remote");
+		System.out.println("in master second method");
+	
 	}
 
 
