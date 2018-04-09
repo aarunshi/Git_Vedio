6adc8d007efdff76034e837c2032a2f97c12a462
aarunshi <38101661+aarunshi@users.noreply.github.com>
2018-04-09 22:27:20 +0530

    Update first.java

--- Git_Vedio/src/first.java
+++ Git_Vedio/src/first.java
@@ -3,7 +3,7 @@ public class first {
 	public static void main(String args[])
 	{
 		System.out.println("hi66");
-		System.out.println("in branch1");
+		System.out.println("in master");
 		second s = new second();
 		s.secondmethod();
 	}
