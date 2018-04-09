996b73e3a9bedd0f87fff6df33e2a64a92bcff96
aarunshi <38101661+aarunshi@users.noreply.github.com>
2018-04-09 22:28:46 +0530

    Update first.java

--- Git_Vedio/src/first.java
+++ Git_Vedio/src/first.java
@@ -3,7 +3,9 @@ public class first {
 	public static void main(String args[])
 	{
 		System.out.println("hi");
-		System.out.println("in branch1");
+		System.out.println("in branch2");
+		second sco = new second();
+		sco.secmethod();
 	}
 
 }
